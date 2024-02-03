608b61dff2a483ec16f4ef753c366498decc88b1
Shahin0Sh <shahin.shukriev@hotmail.com>
2024-02-03 18:58:05 +0200

    minor changes

--- Web scraper/client.py
+++ Web scraper/client.py
@@ -1,10 +1,12 @@
 import requests as r 
 from bs4 import BeautifulSoup as bs 
 import os
+import pandas as pd
 
 url = 'https://dev.bg/company/jobs/junior-intern/?_seniority=intern'
+
 page = r.get(url)
-soup = bs(page.content, 'html.parser')
+soup = bs(page.text, 'html.parser')
 results = soup.find(id="jobs")
 alljobs_elements = results.find_all('div', class_='job-list-item')
 
@@ -12,28 +14,23 @@ cwd = os.path.dirname(__file__)
 my_file = '/my_file.csv'
 cwd += '/data' + my_file
 
+clmnns = ['Position','Location','Date','Job Link']
+df = pd.DataFrame(columns=clmnns)
+
 h6_class = 'job-title ab-title-placeholder ab-cb-title-placeholder'
+data = []
 for job in alljobs_elements:
     element = job.find(title='Python')
     if element != None and element['title'] == 'Python':
-        title = job.find('h6', class_= h6_class)
+        title = job.find('h6', class_= h6_class).text
         location = job.find('span', class_='badge').text.split()[0]
         date = ' '.join(job.find('span', class_='date date-with-icon').text.split())
         job_url = job.find_all('a')[0]['href']
+        data.append([title,location,date,job_url])
 
-        flag = os.path.exists(cwd)
-        if flag:
-            with open(cwd, 'a') as file:
-                file.write(f'Position: {title.text}\n')
-                file.write(f'Location: {location}\n')
-                file.write(f'Date: {date}\n')
-                file.write(f'Job link: {job_url.strip()}\n\n')
-        else:
-            os.chdir(os.path.dirname(__file__))
-            os.mkdir('data')
-
-            with open(cwd, 'a') as file:
-                file.write(f'Position: {title.text}\n')
-                file.write(f'Location: {location}\n')
-                file.write(f'Date: {date}\n')
-                file.write(f'Job link: {job_url.strip()}\n\n')            
+if data:
+    for  val in data:
+        lenght = len(df)
+        df.loc[lenght] = val
+        df.to_csv(cwd, index=False, sep=';', encoding='utf-8-sig')
+        print(f'{val} added to csv')
