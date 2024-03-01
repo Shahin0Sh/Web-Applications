78c7ab6269cd51d6192adaa945fbdb760c736a3f
Shahin0Sh <shahin.shukriev@hotmail.com>
2024-03-01 16:03:37 +0200

    main.py: added file

---
+++ Converter/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+import uvicorn
+from routers.rates import rates_router
+
+app = FastAPI(debug=True)
+app.include_router(rates_router)
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', port=8000, reload=True)
