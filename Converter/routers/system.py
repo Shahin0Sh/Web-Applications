dbf3c851a087cdedb9b125e4609bb446c808acd5
Shahin0Sh <shahin.shukriev@hotmail.com>
2024-03-02 15:36:11 +0200

    system.py: endpoints created

--- Converter/routers/system.py
+++ Converter/routers/system.py
@@ -1,9 +1,14 @@
 from fastapi import APIRouter, Response
+from services import system_service as ss
 
+system_router = APIRouter(prefix='/kc_info')
 
-system_router = APIRouter(prefix='/system')
-
-
-@system_router.get('/kc_info')
+@system_router.get('/')
 def get_kc_info():
-    pass
+    
+    return ss.get_data()
+
+@system_router.post('/')
+def upd_data():
+    ss.insert_kc_prices()
+    return Response(status_code=201, content='Data created.')
