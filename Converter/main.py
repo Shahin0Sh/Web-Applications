7f077fe14e067cdd2e896165731f5918a49e5750
Shahin0Sh <shahin.shukriev@hotmail.com>
2024-03-02 15:34:29 +0200

    main.py: upd imports and router

--- Converter/main.py
+++ Converter/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 import uvicorn
-from routers.system import rates_router
+from routers.system import system_router
 
 app = FastAPI(debug=True)
-app.include_router(rates_router)
+app.include_router(system_router)
 
 if __name__ == '__main__':
     uvicorn.run('main:app', port=8000, reload=True)
