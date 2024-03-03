86e4120e4e7c2691b789bd7ee3c523d88c2a4c2c
Shahin0Sh <shahin.shukriev@hotmail.com>
2024-03-03 15:53:44 +0200

    system.py: updates data feature

--- Converter/routers/system.py
+++ Converter/routers/system.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Response
+from fastapi import APIRouter, Response, HTTPException
 from services import system_service as ss
 
 system_router = APIRouter(prefix='/kc_info')
@@ -8,7 +8,17 @@ def get_kc_info():
     
     return ss.get_data()
 
+@system_router.get('/{amount_kc}')
+def get_amount_kc(amount_kc: int):
+    
+    data = ss.get_amount_kc(amount_kc)
+
+    if data is None:
+        raise HTTPException(status_code=404, detail='No such amount found.')
+    return data
+
 @system_router.post('/')
 def upd_data():
-    ss.insert_kc_prices()
-    return Response(status_code=201, content='Data created.')
+    data = ss.upd_kc_prices()
+
+    return Response(status_code=201, content='Data updated.')
