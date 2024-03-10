28228fb7c20ce1012d162b47ca88c70b9a73396d
Shahin0Sh <shahin.shukriev@hotmail.com>
2024-03-10 15:15:46 +0200

    main.py: implemented app

--- Converter/main.py
+++ Converter/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 import uvicorn
 from routers.system import system_router
+from services.system_service import check_data_exists, insert_kc_prices
 
 app = FastAPI(debug=True)
 app.include_router(system_router)
+
+if not check_data_exists():
+    insert_kc_prices()
 
 if __name__ == '__main__':
     uvicorn.run('main:app', port=8000, reload=True)
